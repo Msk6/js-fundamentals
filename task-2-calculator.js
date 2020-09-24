f2d3692b81a0f38351927fc790fc46c88936ace1
Msk6 <Maha.s.kh@hotmail.com>
2020-09-24 15:30:56 +0300

    updated task-2 with all operations

--- task-2-calculator.js
+++ task-2-calculator.js
@@ -1,7 +1,15 @@
 let num1 = 20
-let num2 = 47
-let operation = '*'
+let num2 = 2
+let operation = '%'
 
 if (operation == '*') {
     console.log(`${num1} * ${num2} = ${num1 * num2}`)
+}else if (operation == '+'){
+    console.log(`${num1} + ${num2} = ${num1 + num2}`)
+}else if (operation == '-') {
+    console.log(`${num1} - ${num2} = ${num1 - num2}`)
+} else if (operation == '/') {
+    console.log(`${num1} / ${num2} = ${num1 / num2}`)
+}else if (operation == '%') {
+    console.log(`${num1} % ${num2} = ${num1 % num2}`)
 }
