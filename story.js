34af2366f3260f8dc8d9f34c486b769e8e5e52ba
Msk6 <Maha.s.kh@hotmail.com>
2020-09-24 12:56:22 +0300

    Solution

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = 'Hessa'
+let age = 22
+let hobby = 'Writing'
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`)
